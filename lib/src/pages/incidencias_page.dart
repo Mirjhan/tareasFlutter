f6041bfee27200b736b54455c9b3959ef5840051
Mirjhan <mirjhanmoreyra@gmail.com>
2026-01-05 23:23:52 -0500

    feat: JYMFLUT-9 creacion de lista de incidencias

--- lib/src/pages/incidencias_page.dart
+++ lib/src/pages/incidencias_page.dart
@@ -11,13 +11,37 @@ class IncidenciasPage extends StatelessWidget {
     return GetBuilder<IncidenciasController>(
         init: controller,
         builder: (controller) => Scaffold(
+              appBar: appBar('Lista de incidencias'),
               body: SafeArea(
-                child: Column(
-                  children: [
-                    Text('Hola'),
-                  ],
-                ),
-              ),
+                  child: ListView.builder(
+                padding: EdgeInsets.all(16),
+                itemCount: 20,
+                itemBuilder: (context, index) => cards(),
+              )),
             ));
+  }
+
+  AppBar appBar(String titulo) {
+    return AppBar(
+      title: Text(titulo),
+      centerTitle: true,
+    );
+  }
+
+  Widget cards() {
+    return Card(
+      elevation: 5,
+      shadowColor: Colors.amber,
+      color: Colors.lightBlue,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      margin: EdgeInsets.only(bottom: 10),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text('Titulo....'), Text('Descripcion....')],
+        ),
+      ),
+    );
   }
 }
